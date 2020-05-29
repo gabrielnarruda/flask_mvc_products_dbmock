c90b0e5e2f932afc8c2a275e549f5ab2dd346462
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-05-28 21:44:52 -0300

    1st commit

---
+++ swagger_server/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import connexion
+
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    app.add_api('swagger.yaml')
+    app.run(port=8080)
+
+
+if __name__ == '__main__':
+    main()
---
+++ swagger_server/controllers/produtos_controller.py
@@ -0,0 +1,24 @@
+def get_listar_produto_por_id(id_produto):  # noqa: E501
+    """Retorna produto por ID
+
+    Retorna produto por ID # noqa: E501
+
+    :param id_produto: Busca por tabela_origem_dominio do domínio
+    :type id_produto: int
+
+    :rtype: Produto
+    """
+    return 'do some magic!'
+
+
+def get_listar_produtos(black_friday=None):  # noqa: E501
+    """Retorna lista de produtos completa
+
+    Retorna lista de produtos completa # noqa: E501
+
+    :param black_friday: Busca por produtos que fazem parte da Black Friday
+    :type black_friday: bool
+
+    :rtype: ProdutoVO
+    """
+    return 'do some magic!'
