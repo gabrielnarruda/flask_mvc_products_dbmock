331363119d766eebae77d133312102f4d54b1597
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-05-31 02:53:53 -0300

    logger 1st try

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -5,6 +5,7 @@ from swagger_server.configuration.logger import create_logger
 
 db_conn = './db_produtos.db'
 
+#logger = create_logger()
 
 def main():
     app = connexion.FlaskApp(__name__, specification_dir='./swagger/')
